84afae7d2aaf67a4f0cdbd0e54580064a39a14cf
dalythu <dalythu@gmail.com>
2025-12-09 16:42:20 -0500

    first step in second problem

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-let fizz = "Fizz";
-let buzz = "Buzz";
-let fizzBuzz = `${fizz} ${buzz}`;
+// let fizz = "Fizz";
+// let buzz = "Buzz";
+// let fizzBuzz = `${fizz} ${buzz}`;
 
-// Part 1: Fizz Buzz
+// // Part 1: Fizz Buzz
 
-for ( let i=1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log(`${i} ${fizzBuzz}`);
-    } else if (i % 5 === 0) {
-        console.log(`${i} ${buzz}`);
-    } else if (i % 3 === 0  ) {
-        console.log(`${i} ${fizz}`);
-    } else {
-        console.log(i);
-    }
+// for ( let i=1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log(`${i} ${fizzBuzz}`);
+//     } else if (i % 5 === 0) {
+//         console.log(`${i} ${buzz}`);
+//     } else if (i % 3 === 0  ) {
+//         console.log(`${i} ${fizz}`);
+//     } else {
+//         console.log(i);
+//     }
 
-};
+// };
 
+// Part 2: Prime Time
 
+for (let n = 1; n <= 10; n++) {
+    console.log(n);
+}

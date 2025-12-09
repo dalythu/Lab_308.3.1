9d1f152df77ee79d6dec0d5f83cc52ac0a2c8e63
dalythu <dalythu@gmail.com>
2025-12-09 14:47:41 -0500

    added first two solutions to the lab to step one of the lab

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+let fizz = "Fizz";
+let buzz = "Buzz";
+let fizzBuzz = `${fizz} ${buzz}`;
+
 // for loop iterate from 1 - 100
 
 for ( let i=1; i <= 100; i++) {
-    console.log(i)
+    if (i % 3 === 0 ) {
+        console.log(`${i} ${fizz}`);
+    } else if (i % 5 === 0) {
+        console.log(`${i} ${buzz}`);
+        } 
 };
 
 
